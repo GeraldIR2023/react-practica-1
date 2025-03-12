def2d533716fb2ae71540857efb911a6e837470c
Gerald Corrales <geraldcorrales29@gmail.com>
2025-03-12 14:52:46 -0600

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import express from "express"; //ESM Ecmascript modules
 import "dotenv/config";
+import cors from "cors";
 import router from "./router";
+import express from "express"; //ESM Ecmascript modules
 import { connectDB } from "./config/db";
-
-const app = express(); //^ Instancia del servidor
+import { corsConfig } from "./config/cors";
 
 connectDB(); //*Conectamos a la base de datos
 
-//*Leer datos de formularios
+const app = express(); //^ Instancia del servidor
+
+//^Configuración de CORS
+app.use(cors(corsConfig));
+
+//^Leer datos de formularios
 app.use(express.json());
 
 app.use("/", router); //*Cuando hacemos una petición a la URL principal, ejecuta el router y entra y mapea los metodos que tenga
